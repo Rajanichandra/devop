1dfa6f3a2f941a28c07a9a9b65945e5092bcc9e1
rajanichandra <rajanikasa.94@gmailcom>
2023-01-23 11:18:54 +0000

    master code

--- demo.java
+++ demo.java
@@ -1,8 +1,15 @@
 class Demo
 {
-  public static void main(....)
+  public static void main(branch code)
   {
     system.out.print(" Hello world ");
     system.out.print(" Hello world ");
   }
+
 }
+
+public static void main(master code)
+{
+	system.out.print("Hello devops");
+	system.out.print("Hello AWS");
+}	
